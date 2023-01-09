33513e50facbed0eea6281b7292ddecf04895094
gopal <gopal1409@gmail.com>
2023-01-09 16:43:09 +0530

    added a new commit

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 });
 
 // Add any custom config to be passed to Jest
+// Add any custom config to be passed to Jest
 const customJestConfig = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
